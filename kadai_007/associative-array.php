3d8bb4071c50a9a1e8eb7907591da067b3aa9923
Lemo0924 <miremon1012@gmail.com>
2025-01-09 01:24:27 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <title>連想配列作成と出力</title>
+</head>
+
+<body>
+  <p>
+    <?php
+     $Array = ['name' => 'onion', 'price' => '200', 'weight' => '160',];
+
+     print_r ($Array);
+
+     ?>
+  </p>
+</body>
+</html>
